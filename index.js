85a3d18118aa072562f00bcb58841b9f392d33fe
Shadab Tahvildary <shtahvildary@gmail.com>
2020-09-25 12:45:14 +0330

    data labels

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const si = require('systeminformation');
 //   .then(data => console.log(data))
 //   .catch(error => console.error(error));
 si.cpuCurrentspeed()
-.then(data => console.log(data))
+.then(data => console.log("cpuCurrentspeed",data))
   .catch(error => console.error(error));
 
   si.currentLoad()
-.then(data => console.log(data))
+.then(data => console.log("currentLoad",data))
   .catch(error => console.error(error));
 
   si.getAllData()
-.then(data => console.log(data))
+.then(data => console.log("getAllData",data))
   .catch(error => console.error(error));
 
   si.wifiNetworks()
-.then(data => console.log(data))
+.then(data => console.log("wifiNetworks",data))
   .catch(error => console.error(error));
 
   si.networkStats()
-.then(data => console.log(data))
+.then(data => console.log("networkStats",data))
   .catch(error => console.error(error));
 
   si.cpuTemperature()
-.then(data => console.log(data))
+.then(data => console.log("cpuTemperature",data))
   .catch(error => console.error(error));
+
+  
