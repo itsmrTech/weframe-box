2515c9603b42d6a27cda3c2d3bfcde62abbe1f42
Shadab Tahvildary <shtahvildary@gmail.com>
2020-10-27 13:40:48 +0330

    df

--- system.js
+++ system.js
@@ -21,12 +21,12 @@ async function sendSystemInfo() {
         inet_latency: (callback) => {
             si.inetLatency("194.5.193.188")
                 .then(data => {
-                    // console.log("then",data)
+                    console.log("then",data)
                     // console.log("response time in ms", data)
                     callback(null, data)
                 })
                 .catch(error => {
-                    // console.log("catch",error)
+                    console.log("catch",error)
                     callback(error)})
         },
         current_load: (callback) => {
