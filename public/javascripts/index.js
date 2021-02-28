c6f20cc7b87bce87693f40bd6bc000f44db96e25
unknown <shamot.group@gmail.com>
2021-02-28 20:49:45 +0330

    turn port

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -244,7 +244,7 @@ socket.on("signal", (otherSignal) => {
             config:{
                 iceServers:[
                     { urls: 'stun:stun.l.google.com:19302' }, { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
-                    {urls:'numb.viagenie.ca',username:'shamot.group@gmail.com',credentials:'fPZf5GnTJWSAngy'}
+                    {urls:'numb.viagenie.ca:3478',username:'shamot.group@gmail.com',credentials:'fPZf5GnTJWSAngy'}
                 ]
             }
         })
